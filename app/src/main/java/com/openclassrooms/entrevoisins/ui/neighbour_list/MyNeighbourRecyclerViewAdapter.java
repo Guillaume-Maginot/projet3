bf3cf9361aa17d32035e7707188c623998aa7cd7
guillaume-Maginot <g.maginot@gmail.com>
2021-04-19 11:56:03 +0200

    Suppression du toast

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -54,12 +54,12 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
             public void onClick(View v) {
                 EventBus.getDefault().post(new DeleteNeighbourEvent(neighbour));
                 //Affichage de longue durée
-                Context context = v.getContext();
-                CharSequence text = "Hello toast!";
-                int duration = Toast.LENGTH_SHORT;
+          //      Context context = v.getContext();
+          //      CharSequence text = "Hello toast!";
+          //      int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+          //      Toast toast = Toast.makeText(context, text, duration);
+          //      toast.show();
             }
         });
     }
